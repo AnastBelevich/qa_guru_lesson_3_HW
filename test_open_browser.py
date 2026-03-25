926f78494fa382e62854a983e029339df456ac26
Anastasiia Belevich <anast.belevich@gmail.com>
2026-03-25 16:39:48 +0300

    добавила тест на поиск и неуспешный поиск с помощью selene

---
+++ test_open_browser.py
@@ -0,0 +1,18 @@
+import time
+
+from selene import browser, be, by, have
+
+browser.open('https://duckduckgo.com/')
+#if browser.element(by.text('Принять все')).matching(be.visible):
+ #  browser.element(by.text('Принять все')).click()
+browser.element('#searchbox_input').should(be.blank).type('qa.guru').press_enter()
+browser.element('#r1-0 > div.ikg2IXiCD14iVX7AdZo1 > h2 > a > span').should(have.text("Курсы Тестировщиков"))
+time.sleep(5)
+
+browser.open('https://duckduckgo.com/')
+browser.element('#searchbox_input').should(be.blank).type('1цывуа').press_enter()
+
+browser.element('#react-layout > div > div:nth-child(1) > div > div.FMPme3X940xAt4SKPFuw > section.At_VJ9MlrHsSjbfCtz2_.aDtqDaYogch0DyrGTrX6 > div > div > p > span').should(have.text("ничего не найдено."))
+time.sleep(5)
+
+
